1785d530ad2c2f4ab86954476c4a3754bc3ad30c
katgiadla <43877781+katgiadla@users.noreply.github.com>
2018-12-31 00:39:08 +0100

    Add files via upload

--- P5_Katarzyna_Giadla.m
+++ P5_Katarzyna_Giadla.m
@@ -10,11 +10,11 @@ plot(in_value(1, :) ,in_value(2, :), 'b.', in_value(3, :), in_value(4, :), 'g.')
 % d³ugoœci od szerokoœci
 hold on; grid on; %hold - zapamiêtanie aktualnego wykresu,
 % kiedy tworz¹ siê inne wykresy
-% grid - wyœwietlanie linii siatek wykresu
+% grid - wyœwietlanie linii siatek wykresu0
 
 dimensions = [12 12]; % wymiary wektora  
-coverstep = 60; %etapy szkolenia w celu pokrycia przestrzeni wejœciowej
-initNeighbor = 5; % wejœciowy rozmiar s¹siedztwa
+coverstep = 50; %etapy szkolenia w celu pokrycia przestrzeni wejœciowej
+initNeighbor = 0; % wejœciowy rozmiar s¹siedztwa
 topologyFcn = 'hextop'; %funkcja topologiczna -> kszta³t, jaki bêd¹ przyjmowaæ nasze dane
 % mog¹ przyjmowaæ kszta³t trójk¹ta, siatek kwadratowych, szeœciok¹tów, itp.
 distanceFcn = 'dist'; %funkcja dystansu neronów - miara euklidesowa (znormalizowana)
@@ -24,5 +24,6 @@ distanceFcn = 'dist'; %funkcja dystansu neronów - miara euklidesowa (znormalizow
 net = selforgmap(dimensions, coverstep, initNeighbor, topologyFcn, distanceFcn); %tworzenie mapy samoorganizacji 
 net.trainFcn = 'trainbu'; %uczenie bez nauczyciela
 net.trainParam.epochs = 600;
+net.trainParam.lr = 0.9; %wspó³czynnik uczenia
 [net, tr] = train(net, in_value); %trening sieci
 y = net(in_value); %testowanie i zapis wyników osi¹gniêtych przez sieæ
